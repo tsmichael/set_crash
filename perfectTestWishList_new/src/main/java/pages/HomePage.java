38617095eb449c6f5dcec3a39f0f73fbbda56ed5
Max Plotitsyn <karrtopelka@gmail.com>
2019-11-10 22:24:49 +0200

    Max Karrtopelka. - Added comparison buttons - Added comparison page - Added Apache Maven Compiler Plugin - Added comparison class and test P.S. Test "ComparisonTest" passed.

--- perfectTestWishList_new/src/main/java/pages/HomePage.java
+++ perfectTestWishList_new/src/main/java/pages/HomePage.java
@@ -2,29 +2,43 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends BasePage{
-
-    public HomePage(WebDriver driver){
-        super(driver);
-    }
+public class HomePage extends BasePage {
+    By buttonSearch = By.xpath("//button/i[contains(@class,'fa-search')]");
 
     String URL = "https://demo.opencart.com";
     By searchField = By.xpath("//input[contains(@class,'input-lg')]");
-    By buttonSearch= By.xpath("//button/i[contains(@class,'fa-search')]");
+    By contactUs = By.cssSelector("a[href$='contact'");
+    // Max Karrtopelka
+    By addToWishListMacBook = By.xpath("//button[@onclick=\"compare.add('43');\"]");
+    By addToWishListiPhone = By.xpath("//button[@onclick=\"compare.add('40');\"]");
+    By addToWishListiMac = By.xpath("//button[@onclick=\"compare.add('42');\"]");
+    By addToWishListCanon = By.xpath("//button[@onclick=\"compare.add('30');\"]");
+    By buttonProductComparison = By.cssSelector("a[href$='compare'");
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
     public HomePage goToOpencart() {
         driver.get(URL);
         return this;
     }
 
-    public StorePage searchItem(String product){
-        writeText(searchField,product);
+    public StorePage searchItem(String product) {
+        writeText(searchField, product);
         click(buttonSearch);
         return new StorePage(driver);
     }
 
-    // Roman Skaskiv - done
-    // Max Plotitsyn - done
+    public ComparisonPage addItem() {
+        click(addToWishListMacBook);
+        click(addToWishListiPhone);
+        click(addToWishListiMac);
+        click(addToWishListCanon);
+        click(buttonProductComparison);
+        return new ComparisonPage(driver);
+    }
 }
