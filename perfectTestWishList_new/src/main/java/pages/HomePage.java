9edca7f256b6d6ccc5afe97f1c9d2e40cad8fd60
Mykhailo Tsalan <pmtsmichael@gmail.com>
2019-11-14 22:31:54 +0200

    nthg new

--- perfectTestWishList_new/src/main/java/pages/HomePage.java
+++ perfectTestWishList_new/src/main/java/pages/HomePage.java
@@ -2,8 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
+
 
 public class HomePage extends BasePage {
     By buttonSearch = By.xpath("//button/i[contains(@class,'fa-search')]");
--- perfectTestWishList_new/src/test/java/test/HomeTest.java
+++ perfectTestWishList_new/src/test/java/test/HomeTest.java
@@ -5,6 +5,7 @@ import pages.HomePage;
 
 public class HomeTest extends BaseTest {
     @Test
+//    @Description("Title of TestCase from TestRail")
     public void testMethod() {
         HomePage homePage = new HomePage(driver);
 
